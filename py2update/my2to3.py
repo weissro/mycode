a3a2b721f0520a9e60959e4a5ecb39a3f628135b
Mona Lisa <monalisa@alta3.com>
2019-05-15 14:29:54 -0400

    python2 to python3

---
+++ py2update/my2to3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+thisround = 0
+while(True):
+    print("What is the IPv4 address used to broadcast on a local network? ")
+    answer = input()
+    thisround = thisround + 1
+    if (answer == '255.255.255.255'):
+        print("Correct!")
+        break
+    elif (thisround == 3):
+        print("Sorry, the answer was 255.255.255.255.")
+        break
+    else: 
+        print("Sorry, try again.")
+
